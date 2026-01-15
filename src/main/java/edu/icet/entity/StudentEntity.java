aba3b03c6f285f9d1b3e017be17eacfb8a706d41
hashini0123 <nanayakkarahashini5@gmail.com>
2026-01-15 16:17:05 +0530

    add modelmapper

--- src/main/java/edu/icet/entity/StudentEntity.java
+++ src/main/java/edu/icet/entity/StudentEntity.java
@@ -1,6 +1,14 @@
 package edu.icet.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String address;
--- src/main/java/edu/icet/service/Impl/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/StudentServiceImpl.java
@@ -1,15 +1,28 @@
 package edu.icet.service.Impl;
 
 import edu.icet.dto.StudentDto;
+import edu.icet.entity.StudentEntity;
+import edu.icet.repository.StudentRepository;
 import edu.icet.service.StudentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    StudentRepository studentRepository;
+
     @Override
     public void addStudent(StudentDto student) {
-
+        StudentEntity entity=modelMapper.map(student, StudentEntity.class);
+        studentRepository.save(entity);
     }
 
     @Override
